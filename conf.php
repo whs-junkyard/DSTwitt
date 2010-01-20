6a5b4d694a4214d02f8a38ff9185211db1d05122
Manatsawin Hanmongkolchai <manatsawin@gmail.com>
2010-01-20 04:04:28 -0700

    [project @ manatsawin@gmail.com-20100120110428-84ew2pr215kks6f4] - Added Favourite support   - Favorited tweet will have heart symbox at front (not tested)   - Favorite timeline - Added unfollow button with confirm() (not tested) - Bug fixed: sizzle.js will now load in mentions timeline. - DSTwitt now loads 20 tweets by default but soft-limited   to display 10 tweets. (can be configured in conf.php) - conf.php new options   - blockedclients: Do not display tweets from these clients                     client name will be strip_tags-ed before matching   - hideuser: Do not display tweets from these display_name   - display: Tweet to display per page

--- conf.php
+++ conf.php
@@ -1,6 +1,12 @@
 <?php
 define("CONSUMER_KEY", "TFehjBocpqYRsaUe92uKgA");
 define("CONSUMER_SECRET", "n1OHA5lwzNpFiM4PNrmpJLMHJQNInJ1JWkEtpvHc");
+$_conf = array();
+$_conf['blockedclients'] = array(
+	"foursquare"
+);
+$_conf['hideuser'] = array();
+$_conf['display'] = 10; //max = 15
 
 // users parser
 $_user = (array) json_decode(file_get_contents("userdb"));
--- tw.php
+++ tw.php
@@ -15,7 +15,7 @@ $oldtweet = json_decode(file_get_contents("cache"));
 $lastupdate = $oldtweet->status;
 $lastupdate = $lastupdate[0];
 php?>
-<script src="../sizzle.js"></script>
+<script src="../<?php if($_GET['timeline']) print "../"; ?>sizzle.js"></script>
 <style>
 body{font-family: sans-serif;}
 .g{color:gray;}
@@ -58,7 +58,7 @@ function actbar(e){
 }
 </script>
 <title>DSTwitt</title>
-<div id="r"><span>@<?=$user?></span> | <a href="/u/<?=$user?>?rnd=<?=uniqid()?>">Home</a> | <a href="/u/<?=$user?>/replies?rnd=<?=uniqid()?>">Mentions</a>
+<div id="r"><span>@<?=$user?></span> | <a href="/u/<?=$user?>?rnd=<?=uniqid()?>">Home</a> | <a href="/u/<?=$user?>/replies?rnd=<?=uniqid()?>">Mentions</a> | <a href="/u/<?=$user?>/fav?rnd=<?=uniqid()?>">Favourite</a>
 	<?php if(!$_SESSION['ssr']){ ?><span class="ssronly"> | <a href='/u/<?=$user?>?norefresh=true&ssr=true'>Text-only mode</a></span><?php } ?>
 	<?php if($_SESSION['ssr']){ ?><span class="deskonly"> | <a href='/u/<?=$user?>?norefresh=true&ssr=false'>Image mode</a></span><?php } ?>
  | <?=date("g:i:s A")?></div>
@@ -73,31 +73,46 @@ function actbar(e){
 flush();
 $tw = new TwitterOAuth(CONSUMER_KEY, CONSUMER_SECRET, $_user[$user]->oauth_token, $_user[$user]->oauth_token_secret);
 if($_GET['rt']){
-	$out = $tw->post("statuses/retweet/".$_GET['rt'], array());
+	$tw->post("statuses/retweet/".$_GET['rt'], array());
+}
+if($_GET['fav']){
+	$tw->post("favorites/create/".$_GET['fav'], array());
+}
+if($_GET['unfav']){
+	$tw->post("favorites/destroy/".$_GET['unfav'], array());
+}
+if($_GET['unfollow']){
+	$tw->post("friendships/destroy/".$_GET['unfollow'], array());
 }
 if($_POST['tweet'] && $_POST['act'] == "Tweet"){
 	$in = array("status" => $_POST['tweet']);
 	if($_POST['irp']) $in['in_reply_to_status_id'] = $_POST['irp'];
-	$out = $tw->post("statuses/update", $in);
+	$tw->post("statuses/update", $in);
 }
 // img.php seems to malfunction without status object.
 // (of course I edited img.php. Also, the earlier version of DSTwitt that use saved password
 // use the old API and it put everything into the status key
 if(!$_GET['norefresh']){
 	if($_POST['act'] == "User timeline"){
-		$tweet = (object) array("status" => $tw->get("statuses/user_timeline", array("count" => 10, "screen_name" => $_POST['tweet'])));
+		$tweet = (object) array("status" => $tw->get("statuses/user_timeline", array("count" => 15, "screen_name" => $_POST['tweet'])));
 	}else if($_GET['timeline'] == "replies"){
-		$tweet = (object) array("status" => $tw->get("statuses/mentions", array("count" => 10)));
+		$tweet = (object) array("status" => $tw->get("statuses/mentions", array("count" => 20)));
+	}else if($_GET['timeline'] == "fav"){
+		$tweet = (object) array("status" => $tw->get("favorites", array()));
 	}else{
-		$tweet = (object) array("status" => $tw->get("statuses/home_timeline", array("count" => 10)));
+		$tweet = (object) array("status" => $tw->get("statuses/home_timeline", array("count" => 15)));
 	}
 }else{
 	$tweet = json_decode(file_get_contents("cache"));
 }
 file_put_contents("cache", json_encode((array) $tweet));
+$i=0;
 foreach($tweet->status as $t){
+	if(in_array(strip_tags($t->source), $_conf['blockedclients'])) continue;
+	if(in_array($t->user->screen_name, $_conf['hideuser'])) continue;
 	print '<li onclick="actbar(this);">';
 	if($t->id == $lastupdate->id) print "<b>&gt;&gt;&gt;</b> ";
+	if($t->favorited) print "&#x2661;";
 	if($_SESSION['ssr']){
 		$client = strip_tags($t->source);
 		$ti = date("g:i:s A", strtotime($t->created_at));
@@ -107,14 +122,20 @@ foreach($tweet->status as $t){
 		$ti = date("g:i:s A", strtotime($t->created_at));
 		print $t->user->screen_name.' <img src="/t/'.$t->id.'" />  ('.$ti.' | '.$client.')';
 	}
-	echo '<div class="actionbar"><a href="#" onclick="t(\''.$t->user->screen_name.'\', '.$t->id.'); return false;"><button>@</button></a>';
+	echo '<div class="actionbar">';
+	print '<a href="'.$fnpath.'?unfollow='.$t->user->id.'" onclick="return confirm(\'Unfollow?\')"><button>Unfollow</button></a>';
+	print '<a href="#" onclick="t(\''.$t->user->screen_name.'\', '.$t->id.'); return false;"><button>@</button></a>';
 	print '<a href="'.$fnpath.'?rt='.$t->id.'"><button>RT</button></a>';
 	print '<a href="#" onclick="ut(\''.$t->user->screen_name.'\'); return false;"><button>TL</button></a>';
+	if($t->favorited) print '<a href="'.$fnpath.'?unfav='.$t->id.'"><button><b>Faved</b></button></a>';
+	else print '<a href="'.$fnpath.'?fav='.$t->id.'"><button>Fave</button></a>';
 	if(preg_match("~http://twitpic.com/([^ ]+)~", $t->text, $twtpic)){
 		print " <a href='/twitpic/".$twtpic[1]."'>TwitPic</a> ";
 	}
 	print '</div></li>';
 	flush();
+	$i++;
+	if($i > $_conf['display']) break;
 }
 php?>
 </ul>
